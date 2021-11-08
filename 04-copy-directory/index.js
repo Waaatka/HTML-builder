c87cbb1d5d588eaaa8aa8032df036b336f7c002f
Anna Sapegina <watochka@gmail.com>
2021-11-08 20:55:21 +0300

    feat: copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const { promises: fs } = require('fs');
+const path = require('path');
 
+const copyPath = path.resolve(__dirname, 'files-copy');
+const originalPath = path.resolve(__dirname, 'files');
+fs.rm(copyPath, { recursive: true, force: true })
+  .then(() => fs.mkdir(copyPath))
+  .then(() => fs.readdir(originalPath))
+  .then((files) =>
+    Promise.all(
+      files.map((file) =>
+        fs.copyFile(path.join(originalPath, file), path.join(copyPath, file))
+      )
+    )
+  );
